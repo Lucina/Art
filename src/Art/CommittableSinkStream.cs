5a1bbaadd91c346c03d6a5d358c25f7cee6b750d
Lucina <10523871+Lucina@users.noreply.github.com>
2022-02-16 23:24:48 -0800

    Make sink streams properly sink

--- src/Art/CommittableSinkStream.cs
+++ src/Art/CommittableSinkStream.cs
@@ -3,12 +3,69 @@ namespace Art;
 /// <summary>
 /// Represents a wrapper around a <see cref="SinkStream"/>.
 /// </summary>
-public class CommittableSinkStream : CommittableMemoryStream
+public class CommittableSinkStream : CommittableStream
 {
-    /// <summary>
-    /// Creates a new instance of <see cref="CommittableSinkStream"/>.
-    /// </summary>
-    public CommittableSinkStream() : base(new SinkStream())
+    /// <inheritdoc />
+    public override void Flush()
+    {
+    }
+
+    /// <inheritdoc />
+    public override int Read(byte[] buffer, int offset, int count) => throw new NotSupportedException();
+
+    /// <inheritdoc />
+    public override long Seek(long offset, SeekOrigin origin) => throw new NotSupportedException();
+
+    /// <inheritdoc />
+    public override void SetLength(long value) => throw new NotSupportedException();
+
+    /// <inheritdoc />
+    public override void WriteByte(byte value)
+    {
+    }
+
+    /// <inheritdoc />
+    public override void Write(byte[] buffer, int offset, int count) => ValidateBufferArguments(buffer, offset, count);
+
+    /// <inheritdoc />
+    public override void Write(ReadOnlySpan<byte> buffer)
+    {
+    }
+
+    /// <inheritdoc />
+    public override Task WriteAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+    {
+        ValidateBufferArguments(buffer, offset, count);
+        return Task.CompletedTask;
+    }
+
+    /// <inheritdoc />
+    public override ValueTask WriteAsync(ReadOnlyMemory<byte> buffer, CancellationToken cancellationToken = default)
+    {
+        return ValueTask.CompletedTask;
+    }
+
+    /// <inheritdoc />
+    public override bool CanRead => false;
+
+    /// <inheritdoc />
+    public override bool CanSeek => false;
+
+    /// <inheritdoc />
+    public override bool CanWrite => true;
+
+    /// <inheritdoc />
+    public override long Length => throw new NotSupportedException();
+
+    /// <inheritdoc />
+    public override long Position
+    {
+        get { throw new NotSupportedException(); }
+        set { throw new NotSupportedException(); }
+    }
+
+    /// <inheritdoc />
+    protected override void Commit()
     {
     }
 }
