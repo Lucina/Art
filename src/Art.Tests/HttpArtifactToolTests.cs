010d9e2e79d373b56efbab1bd2a3f33486612bec
riina <54872398+riina@users.noreply.github.com>
2022-02-25 03:20:17 -0800

    Update HttpArtifactToolTests.cs

--- src/Art.Tests/HttpArtifactToolTests.cs
+++ src/Art.Tests/HttpArtifactToolTests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Text.Json;
 using System.Threading.Tasks;
 using NUnit.Framework;
@@ -8,7 +9,7 @@ namespace Art.Tests;
 public class HttpArtifactToolTests : HttpTestsBase
 {
     [Test]
-    public async Task GetDeserializedJsonAsync_NullJson_IsNull()
+    public async Task GetDeserializedJsonAsync_NullJson_Null()
     {
         MockHandler.When("http://localhost/imaginaryfriend.json").Respond("application/json", "null");
         var x = await Tool.GetDeserializedJsonAsync<JsonElement?>("http://localhost/imaginaryfriend.json");
@@ -16,10 +17,26 @@ public class HttpArtifactToolTests : HttpTestsBase
     }
 
     [Test]
-    public async Task GetDeserializedJsonAsync_NotNullJson_IsNotNull()
+    public async Task GetDeserializedJsonAsync_NotNullJson_NotNull()
     {
         MockHandler.When("http://localhost/watashironri.json").Respond("application/json", @"{""kaf"":""epic""}");
         var x = await Tool.GetDeserializedJsonAsync<JsonElement?>("http://localhost/watashironri.json");
         Assert.That(x, Is.Not.Null);
+    }
+
+    [Test]
+    public async Task GetDeserializedRequiredJsonAsync_NullJsonToJsonElement_NullValue()
+    {
+        MockHandler.When("http://localhost/imaginaryfriend.json").Respond("application/json", "null");
+        JsonElement res = await Tool.GetDeserializedRequiredJsonAsync<JsonElement>("http://localhost/imaginaryfriend.json");
+        Assert.That(res.ValueKind, Is.EqualTo(JsonValueKind.Null));
+    }
+
+    [Test]
+    public void GetDeserializedRequiredJsonAsync_NullJsonToNullableJsonElement_Throws()
+    {
+        MockHandler.When("http://localhost/imaginaryfriend.json").Respond("application/json", "null");
+        Assert.That(() => Tool.GetDeserializedRequiredJsonAsync<JsonElement?>("http://localhost/imaginaryfriend.json").Wait(),
+            Throws.InstanceOf<AggregateException>().With.InnerException.InstanceOf<NullJsonDataException>());
     }
 }
