52359e066b3c6cd28517b4d08103b570e4f86fe9
riina <54872398+riina@users.noreply.github.com>
2022-03-04 15:00:26 -0800

    Add EF migration for retrieval date

--- src/Art.EF.Sqlite/Migrations/20220304225855_AddRetrievalDates.Designer.cs
+++ src/Art.EF.Sqlite/Migrations/20220304225855_AddRetrievalDates.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Art.EF;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Art.EF.Sqlite.Migrations
 {
     [DbContext(typeof(ArtifactContext))]
-    partial class ArtifactContextModelSnapshot : ModelSnapshot
+    [Migration("20220304225855_AddRetrievalDates")]
+    partial class AddRetrievalDates
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.2");
---
+++ src/Art.EF.Sqlite/Migrations/20220304225855_AddRetrievalDates.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Art.EF.Sqlite.Migrations
+{
+    public partial class AddRetrievalDates : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "Retrieved",
+                table: "ArtifactResourceInfoModels",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "RetrievalDate",
+                table: "ArtifactInfoModels",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Retrieved",
+                table: "ArtifactResourceInfoModels");
+
+            migrationBuilder.DropColumn(
+                name: "RetrievalDate",
+                table: "ArtifactInfoModels");
+        }
+    }
+}
