d48a1673d680915a123074c04feee9ba2078422d
v69ar73aqvc <54872398+v69ar73aqvc@users.noreply.github.com>
2023-01-29 01:54:11 -0800

    Update ISinkStream.cs

--- src/Art/ISinkStream.cs
+++ src/Art/ISinkStream.cs
@@ -3,6 +3,7 @@
 /// <summary>
 /// Tag interface for <see cref="Stream"/> types that are a no-op for output.
 /// </summary>
+/// <remarks>This should be kept in the shared assembly, since this could potentially be needed across plugin / host bounds.</remarks>
 public interface ISinkStream
 {
 }
