49cd6cba4fd68a7ebb25e3e62ac0956755eeef66
riina <54872398+riina@users.noreply.github.com>
2023-02-25 08:33:05 -0800

    add tesler and modular

---
+++ src/Art.Modular/ManifestNotFoundException.cs
@@ -0,0 +1,13 @@
+﻿namespace Art.Modular;
+
+public class ManifestNotFoundException : ArtUserException
+{
+    public string AssemblyName { get; }
+
+    public ManifestNotFoundException(string assemblyName)
+    {
+        AssemblyName = assemblyName;
+    }
+
+    public override string Message => $"No applicable manifest for the assembly {AssemblyName} could be found.";
+}
