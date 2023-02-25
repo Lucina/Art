c75c05a395de46ff156fabdf9d055e0b2af5cb7f
riina <54872398+riina@users.noreply.github.com>
2023-02-25 08:22:41 -0800

    Add registry stores

---
+++ src/Art.Common/IRegistryStore.cs
@@ -0,0 +1,20 @@
+﻿namespace Art.Common;
+
+/// <summary>
+/// Represents a store of <see cref="IArtifactToolRegistry"/>.
+/// </summary>
+public interface IRegistryStore
+{
+    /// <summary>
+    /// Loads a registry that applies to the specified tool ID.
+    /// </summary>
+    /// <param name="artifactToolId">Tool ID to get a registry for.</param>
+    /// <returns>Registry.</returns>
+    IArtifactToolRegistry LoadRegistry(ArtifactToolID artifactToolId);
+
+    /// <summary>
+    /// Loads all available registries in this store.
+    /// </summary>
+    /// <returns>Registry sequence.</returns>
+    IEnumerable<IArtifactToolRegistry> LoadAllRegistries();
+}
---
+++ src/Art.Common/StaticRegistryStore.cs
@@ -0,0 +1,31 @@
+﻿namespace Art.Common;
+
+/// <summary>
+/// Represents a store of a single <see cref="IArtifactToolRegistry"/>.
+/// </summary>
+public class StaticRegistryStore : IRegistryStore
+{
+    private readonly IArtifactToolRegistry _artifactToolRegistry;
+
+    /// <summary>
+    /// Initializes an instance of <see cref="StaticRegistryStore"/>.
+    /// </summary>
+    /// <param name="artifactToolRegistry">Registry to use.</param>
+    public StaticRegistryStore(IArtifactToolRegistry artifactToolRegistry) => _artifactToolRegistry = artifactToolRegistry;
+
+    /// <inheritdoc />
+    public IArtifactToolRegistry LoadRegistry(ArtifactToolID artifactToolId)
+    {
+        if (_artifactToolRegistry.Contains(artifactToolId))
+        {
+            throw new ArtUserException($"Registry does not contain an artifact with the ID {artifactToolId}");
+        }
+        return _artifactToolRegistry;
+    }
+
+    /// <inheritdoc />
+    public IEnumerable<IArtifactToolRegistry> LoadAllRegistries()
+    {
+        yield return _artifactToolRegistry;
+    }
+}
