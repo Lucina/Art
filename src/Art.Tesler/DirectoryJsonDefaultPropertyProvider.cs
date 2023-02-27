4581e3280d9a4d5ca45b1582933b774a7f72e8e9
riina <54872398+riina@users.noreply.github.com>
2023-02-27 10:41:47 -0800

    add common property file

--- src/Art.Tesler/DirectoryJsonDefaultPropertyProvider.cs
+++ src/Art.Tesler/DirectoryJsonDefaultPropertyProvider.cs
@@ -3,23 +3,41 @@ using Art.Common;
 
 namespace Art.Tesler;
 
-public class DirectoryDefaultPropertyProvider : IDefaultPropertyProvider
+public class DirectoryJsonDefaultPropertyProvider : IDefaultPropertyProvider
 {
     private readonly string _directory;
+    private readonly IReadOnlyDictionary<string, JsonElement>? _commonProperties;
 
-    public DirectoryDefaultPropertyProvider(string directory)
+    public DirectoryJsonDefaultPropertyProvider(string directory, string? commonPropertyFile = null)
     {
         if (!Directory.Exists(directory))
         {
             throw new DirectoryNotFoundException($"Configuration directory \"{directory}\" does not exist");
         }
         _directory = directory;
+        if (commonPropertyFile != null)
+        {
+            commonPropertyFile = Path.Combine(_directory, commonPropertyFile);
+            if (!File.Exists(commonPropertyFile))
+            {
+                throw new FileNotFoundException($"Could not find common property file {commonPropertyFile}");
+            }
+            using var fs = File.OpenRead(commonPropertyFile);
+            _commonProperties = JsonSerializer.Deserialize(fs, SourceGenerationContext.Default.IReadOnlyDictionaryStringJsonElement);
+        }
     }
 
     public void WriteDefaultProperties(ArtifactToolID artifactToolId, Dictionary<string, JsonElement> dictionary)
     {
         string toolNameSafe = artifactToolId.ToolString.SafeifyFileName();
         string toolFile = Path.Combine(_directory, $"{toolNameSafe}.json");
+        if (_commonProperties != null)
+        {
+            foreach (var pair in _commonProperties)
+            {
+                dictionary[pair.Key] = pair.Value;
+            }
+        }
         if (File.Exists(toolFile))
         {
             using var file = File.OpenRead(toolFile);
