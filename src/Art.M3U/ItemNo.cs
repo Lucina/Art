c461d70e8c16c1188d6a700dafd214a7e63a81ee
riina <54872398+riina@users.noreply.github.com>
2023-03-05 03:32:25 -0800

    add ellipsis handler

---
+++ src/Art.M3U/ItemNo.cs
@@ -0,0 +1,15 @@
+﻿namespace Art.M3U;
+
+/// <summary>
+/// Marks item number.
+/// </summary>
+/// <param name="Number">Item number.</param>
+/// <param name="Total">Total.</param>
+public readonly record struct ItemNo(long Number, long? Total)
+{
+    /// <summary>
+    /// Gets message.
+    /// </summary>
+    /// <returns>Message.</returns>
+    public string GetMessage() => $"{Number:D}/{Total?.ToString("D") ?? "???"}";
+}
---
+++ src/BarHelper/EllipsisSuffixContentFiller.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+namespace BarHelper;
+
+public class EllipsisSuffixContentFiller : IContentFiller
+{
+    public static EllipsisSuffixContentFiller Create(string message, int initialI)
+    {
+        return new EllipsisSuffixContentFiller(message, initialI);
+    }
+
+    public readonly string Message;
+    private readonly StringBuilder _content;
+    private int _i;
+
+    public EllipsisSuffixContentFiller(string message, int initialI)
+    {
+        Message = message;
+        _content = new StringBuilder(Message);
+        _i = initialI;
+    }
+
+    public void Fill(StringBuilder stringBuilder, int width)
+    {
+        _content.Length = Message.Length;
+        _content.Append('.', _i + 1);
+        _i = (_i + 1) % 3;
+        StringFillUtil.FillLeft(_content.ToString(), stringBuilder, width);
+    }
+}
