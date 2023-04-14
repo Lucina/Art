acce440435f65687911a86e3e2025b7d683ceeaf
riina <54872398+riina@users.noreply.github.com>
2023-04-14 07:25:06 -0700

    add hls stream resource

---
+++ src/Art.M3U/Resources/M3UArtifactDataExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Art.Common;
+
+namespace Art.M3U.Resources;
+
+/// <summary>
+/// Extensions for <see cref="ArtifactData"/>.
+/// </summary>
+public static partial class M3UArtifactDataExtensions
+{
+    /// <summary>
+    /// Creates an <see cref="M3UDownloaderContextSaverArtifactResourceInfo"/> resource.
+    /// </summary>
+    /// <param name="artifactData">Source <see cref="ArtifactData"/> instance.</param>
+    /// <param name="saver">Stream context.</param>
+    /// <param name="key">Resource key.</param>
+    /// <param name="contentType">MIME content type.</param>
+    /// <param name="updated">Updated date.</param>
+    /// <param name="version">Version.</param>
+    /// <param name="checksum">Checksum.</param>
+    public static ArtifactDataResource HLSStream(this ArtifactData artifactData,
+        M3UDownloaderContextStreamOutputSaver saver,
+        ArtifactResourceKey key,
+        string? contentType = "application/octet-stream",
+        DateTimeOffset? updated = null,
+        string? version = null,
+        Checksum? checksum = null)
+        => new(artifactData, new M3UDownloaderContextSaverArtifactResourceInfo(saver, key, contentType, updated, version, checksum));
+}
---
+++ src/Art.M3U/Resources/M3UDownloaderContextSaverArtifactResourceInfo.cs
@@ -0,0 +1,22 @@
+﻿namespace Art.M3U.Resources;
+
+/// <summary>
+/// Represents an HLS stream.
+/// </summary>
+/// <param name="Saver">Stream context.</param>
+/// <param name="Key">Resource key.</param>
+/// <param name="ContentType">MIME content type.</param>
+/// <param name="Updated">Updated date.</param>
+/// <param name="Version">Version.</param>
+/// <param name="Checksum">Checksum.</param>
+public record M3UDownloaderContextSaverArtifactResourceInfo(M3UDownloaderContextStreamOutputSaver Saver, ArtifactResourceKey Key, string? ContentType = "application/octet-stream", DateTimeOffset? Updated = null, string? Version = null, Checksum? Checksum = null) : ArtifactResourceInfo(Key, ContentType, Updated, Version, Checksum)
+{
+    /// <inheritdoc />
+    public override bool CanExportStream => true;
+
+    /// <inheritdoc />
+    public override async ValueTask ExportStreamAsync(Stream targetStream, CancellationToken cancellationToken = default)
+    {
+        await Saver.ExportAsync(targetStream, cancellationToken).ConfigureAwait(false);
+    }
+}
