493a7d17a7597fc7ecf41d399aa494d591abec92
riina <54872398+riina@users.noreply.github.com>
2024-12-08 03:11:08 -0800

    Remove wcunlockA

--- src/wcunlockA/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// dotnet publish -c Release /p:PublishSingleFile=true /p:PublishAot=true /p:DebuggerSupport=false /p:DebugType=None /p:CopyOutputSymbolsToPublishDirectory=false /p:InvariantGlobalization=true /p:OptimizationPreference=size --self-contained
-using System.Security.Cryptography;
-
-using var input = args.Length >= 1 ? new MemoryStream(File.ReadAllBytes(args[0])) : Console.OpenStandardInput();
-using var output = args.Length >= 2 ? File.Create(args[1]) : Console.OpenStandardOutput();
-var ms = new MemoryStream();
-await input.CopyToAsync(ms);
-await output.WriteAsync(ProtectedData.Unprotect(ms.ToArray(), null, DataProtectionScope.CurrentUser));
